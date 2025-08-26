9ac3c34ed7d3cb480ee5b6c14e7ecc15ea20126e
tushar <tusharkokane366@gmail.com>
2025-08-26 23:45:47 +0530

    feat: add the AccountRepository for the account entity.

---
+++ src/main/java/com/finance/tracker/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.finance.tracker.repository;
+
+import com.finance.tracker.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByUserId(Long userId);
+}
